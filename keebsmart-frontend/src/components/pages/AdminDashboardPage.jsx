9b6147b27bca6b38d56696d2aa1aef1b82e74b99
Dionovan_Ramadhani <dionovan7@gmail.com>
2024-07-10 05:57:45 +0700

    update dashboard page for admin

--- keebsmart-frontend/src/components/pages/AdminDashboardPage.jsx
+++ keebsmart-frontend/src/components/pages/AdminDashboardPage.jsx
@@ -1,29 +1,141 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Accordion, AccordionSummary, AccordionDetails,  } from '@mui/material';
+import { ArrowDropDown, AccountCircleOutlined } from '@mui/icons-material';
 
 export default function AdminDashboardPage() {
+    const divSize = (width, height) => ({
+        width, height
+    })
+
     return (
         <div className="flex">
-            <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl bg-gray-400">
+            <aside className="relative w-64 hidden sm:block shadow-xl bg-white">
                 <div className="p-6">
-                    <a className="text-white text-3xl font-semibold uppercase hover:text-gray-300">KeebsMart Admin</a>
-                    <button className="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                        Add New Product
-                    </button>
+                    <a className="text-black text-3xl font-semibold uppercase hover:text-gray-300">KeebsMart Admin</a>
                 </div>
                 <nav className="text-white text-base font-semibold pt-3">
-                    <a href="index.html" className="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
-                        {/* Add dashboard icon */}
-                        Dashboard
-                    </a>
+                    <a href="#" className="flex items-center active-nav-link text-black py-4 pl-6 nav-item">Home</a>
+                    <a href="#" className="flex items-center active-nav-link text-black py-4 pl-6 nav-item">Sales</a>
+                   
+
+                    <Accordion className='mt-2'>
+                        <AccordionSummary expandIcon={<ArrowDropDown />} aria-controls="panel2-content" id="panel2-header" >
+                        <a href='#'>Products</a>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <a href="">Add New Product</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">All Products</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Keyboards</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Keycaps</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Switch</a>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ArrowDropDown />} aria-controls="panel2-content" id="panel2-header" >
+                            <a href='#'>Orders</a>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <a href="">All Orders</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Processed Orders</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">On Delivery</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Canceled Orders</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Finished Orders</a>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ArrowDropDown />} aria-controls="panel2-content" id="panel2-header" >
+                            <a href='#'>User</a>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <a href="">All Users</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Active Users</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Non-Active Users</a>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ArrowDropDown />} aria-controls="panel2-content" id="panel2-header" >
+                            <a href='#'>Product Log</a>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <a href="">All Product Log</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Product In</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Product Out</a>
+                        </AccordionDetails>
+                        <AccordionDetails>
+                            <a href="">Product Update</a>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <a href="#" className="flex items-center active-nav-link text-black py-4 pl-6 nav-item">Chat</a>
                 </nav>
             </aside>
 
-            <div className="w-full flex flex-col p-5">
-                <h1 className="text-4xl font-medium tracking-normal">Dashboard</h1>
-                <p className="text-xl font-light tracking-wide">The place for admins manage our products</p>
-                <div className="mt-8">
-                    <h1 className="font-medium text-2xl tracking-wide">Selling Data</h1>
-                    <TableContainer component={Paper} className='mt-5'>
+            {/* Dashboard */}
+            <div className="w-full flex flex-col p-5 bg-slate-50">
+                <div className="flex justify-between">
+                    <div>
+                        <h1 className="text-4xl font-medium tracking-normal text-slate-500">DASHBOARD</h1>
+                        <p className="text-xl font-light tracking-wide">Welcome back, Bambang!</p>
+                    </div>
+                    <AccountCircleOutlined sx={{width: 50, height: 50}} className='cursor-pointer' />
+
+                </div>
+                <div className="mt-8 grid grid-cols-3 gap-4 mx-auto text-center">
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Sold Products</h1>
+                        <p className='font-medium text-7xl'>20</p>
+                    </div>
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>New Orders</h1>
+                        <p className='font-medium text-7xl'>3</p>
+                    </div>
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Processed Orders</h1>
+                        <p className='font-medium text-7xl'>5</p>
+                    </div>
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Orders in Delivery</h1>
+                        <p className='font-medium text-7xl'>2</p>
+                    </div>
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Canceled Orders</h1>
+                        <p className='font-medium text-7xl'>4</p>
+                    </div>
+                    <div className="border rounded-lg shadow-md p-10 bg-white" style={divSize(350, 280)} >
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Active Products</h1>
+                        <p className='font-medium text-7xl'>4</p>
+                    </div>     
+                    <div className="col-span-3 border rounded-lg shadow-md p-10 bg-white" style={divSize(null, 280)}>
+                        <h1 className='font-sm text-2xl text-slate-500 mb-8 text-slate-500"'>Total Income</h1>
+                        <p className='font-medium text-7xl'>Rp. 500.000.000</p>
+                    </div>
+                    {/* <TableContainer component={Paper} className='mt-5'>
                         <Table sx={{minWidth: 650}} >
                             <TableHead>
                                 <TableRow className='bg-gray-200'>
@@ -45,7 +157,7 @@ export default function AdminDashboardPage() {
                                 </TableRow>
                             </TableBody>
                         </Table>
-                    </TableContainer>
+                    </TableContainer> */}
                 </div>
             </div>
         </div>
